526217a8259bdb2fd2fe2d7ec27d368c21be08b5
DianaGaleano245 <emmaymaggi@gmail.com>
2022-08-31 14:27:31 -0300

    trabajando

--- TP4/Cliente.cs
+++ TP4/Cliente.cs
@@ -1,10 +1,21 @@
+using System;
+using System.Collections.Generic;
 namespace TP4
 {
     public class Cliente
     {
         public string RazonSocial {get; set;}
-        public int cuil{get; set;}
+        public int Cuil{get; set;}
         public List<Proyecto> Proyectos {get; set;}
-
+        public Cliente(string razonSocial, int cuil)
+        {
+            RazonSocial = razonSocial;
+            Cuil = cuil;
+            Proyectos = new List<Proyecto>();
+        }
+        public void AgregarProyecto(Proyecto proyecto)
+        {
+            Proyecto.Add()
+        } 
     }
 }
