474c73e7f9f375ffb1bc5b981549fff89c9a1ea3
luchoxx87 <emmaymaggi@gmail.com>
2022-09-08 15:33:51 -0300

    tp4 cambiamos rubro

--- TP4/TP4.AMySQL/Class1.cs
+++ TP4/TP4.AMySQL/Class1.cs
@@ -1,5 +1,55 @@
 ﻿namespace TP4.AdoMySQL;
 public class Class1
 {
+    
+    public class MapCliente: Mapeador<Cliente>
+    {
+        public MapCliente(AdoAGBD ado):base(ado)
+        {
+            Tabla = "Cliente";
+        }
+        public override Cliente ObjetoDesdeFila(DataRow fila)
+            => new Cliente()
+            {
+                Id = Convert.ToByte(fila["idCliente"]),
+                Nombre = fila["Cliente"].ToString()
+            };
 
-}
+        public void AltaCliente(Cliente cliente)
+            => EjecutarComandoCon("altaCliente", ConfigurarAltaCliente, PostAltaCliente, Cliente);
+
+        public void ConfigurarAltaCliente(Cliente cliente)
+        {
+            SetComandoSP("altaCliente");
+
+            BP.CrearParametroSalida("unIdCliente")
+                .SetTipo(MySql.Data.MySqlClient.MySqlDbType.UByte)
+                .AgregarParametro();
+
+            BP.CrearParametro("unCliente")
+                .SetTipoVarchar(45)
+                .SetValor(Cliente.Nombre)
+                .AgregarParametro();
+        }
+
+        public void PostAltaCliente(Cliente cliente)
+        {
+            var paramIdCliente = GetParametro("unIdCliente");
+            Cliente.Id = Convert.ToByte(paramIdCliente.Value);
+        }
+
+        public Cliente ClientePorId(byte id)
+        {
+            SetComandoSP("ClientePorId");
+
+            BP.CrearParametro("unIdCliente")
+                .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Byte)
+                .SetValor(id)
+                .AgregarParametro();
+
+            return ElementoDesdeSP();
+        }
+
+        public List<Cliente> ObtenerClientes() => ColeccionDesdeTabla();
+    }
+    }
