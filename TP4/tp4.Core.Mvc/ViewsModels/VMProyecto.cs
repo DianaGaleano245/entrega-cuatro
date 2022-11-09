44a71cb0df5c614647761734fd8d67bde2df1910
DianaGaleano245 <emmaymaggi@gmail.com>
2022-11-09 16:57:58 -0300

    proceso2

--- TP4/tp4.Core.Mvc/ViewsModels/VMProyecto.cs
+++ TP4/tp4.Core.Mvc/ViewsModels/VMProyecto.cs
@@ -2,43 +2,42 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using TP4.Core;
 using Microsoft.AspNetCore.Mvc.Rendering;
-namespace tp4.Core.MVC.ViewModels
+namespace tp4.Core.Mvc;
+
+public class VMProyecto
 {
-    public class VMProyecto
+    public SelectList? Clientes { get; set; }
+    public string DescripcionProyecto { get; set; }
+    public Decimal PresupuestoProyecto { get; set; }
+    public DateTime InicioProyecto { get; set; }
+    public DateTime FinProyecto { get; set; }
+
+    [Range(1, byte.MaxValue, ErrorMessage = "Seleccionar un Cliente")]
+    public int Cuit { get; set; }
+
+    public short idProyecto { get; set; }
+
+    public VMProyecto() { }
+    public VMProyecto(IEnumerable<Cliente> clientes)
     {
-        public SelectList? Clientes { get; set; }
-        public string DescripcionProyecto { get; set; }
-        public Decimal PresupuestoProyecto { get; set; }
-        public DateTime InicioProyecto { get; set; }
-        public DateTime FinProyecto { get; set; }
+        Clientes = new SelectList(clientes,
+                                dataTextField: nameof(Cliente.Cuit),
+                                dataValueField: nameof(Cliente.RazonSocial));
+    }
+    public VMProyecto(IEnumerable<Cliente> clientes, Proyecto proyecto)
+    {
 
-        [Range(1, byte.MaxValue, ErrorMessage = "Seleccionar un Cliente")]
-        public int Cuit { get; set; }
+        Clientes = new SelectList(clientes,
+                                dataTextField: nameof(Cliente.Cuit),
+                                dataValueField: nameof(Cliente.RazonSocial),
+                                selectedValue: proyecto.IdProyecto);
+        DescripcionProyecto = proyecto.Descripcion;
+        PresupuestoProyecto = proyecto.Presupuesto;
+        InicioProyecto = proyecto.Inicio;
+        FinProyecto = proyecto.Fin;
+        idProyecto = proyecto.IdProyecto;
 
-        public short idProyecto { get; set; }
-
-        public VMProyecto() { }
-        public VMProyecto(IEnumerable<Cliente> clientes)
-        {
-            Clientes = new SelectList(clientes,
-                                    dataTextField: nameof(Cliente.Cuit),
-                                    dataValueField: nameof(Cliente.RazonSocial));
-        }
-        public VMProyecto(IEnumerable<Cliente> clientes, Proyecto proyecto)
-        {
-
-            Clientes = new SelectList(clientes,
-                                    dataTextField: nameof(Cliente.Cuit),
-                                    dataValueField: nameof(Cliente.RazonSocial),
-                                    selectedValue: proyecto.IdProyecto);
-            DescripcionProyecto = proyecto.Descripcion;
-            PresupuestoProyecto = proyecto.Presupuesto;
-            InicioProyecto = proyecto.Inicio;
-            FinProyecto = proyecto.Fin;
-            idProyecto = proyecto.IdProyecto;
-
-
-        }
 
     }
+
 }
